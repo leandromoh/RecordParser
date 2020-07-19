6aadf99a680f208c4d2e2696b21a11759c571b8b
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2020-07-18 23:23:24 -0300

    added CSVSequentialBuilder

---
+++ RecordParser/Parsers/CSVSequentialBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace RecordParser.Parsers
+{
+    public class CSVSequentialBuilder<T>
+    {
+        private readonly CSVIndexedBuilder<T> indexed = new CSVIndexedBuilder<T>();
+        private int currentIndex = -1;
+
+        public CSVSequentialBuilder<T> Map<R>(
+            Expression<Func<T, R>> ex,
+            Expression<Func<string, R>> convert = null,
+            Expression<Func<string, bool>> skipRecordWhen = null)
+        {
+            indexed.Map(ex, ++currentIndex, convert, skipRecordWhen);
+            return this;
+        }
+
+        public CSVSequentialBuilder<T> Skip(int collumCount)
+        {
+            currentIndex += collumCount;
+            return this;
+        }
+
+        public CSVSequentialBuilder<T> DefaultTypeConvert<R>(Expression<Func<string, R>> ex)
+        {
+            indexed.DefaultTypeConvert(ex);
+            return this;
+        }
+
+        public CSVReader<T> Build() => indexed.Build();
+    }
+}
