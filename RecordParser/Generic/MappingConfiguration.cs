29edd0520de643059d5c9be71633a9f5246332de
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2020-07-20 14:24:31 -0300

    change MappingConfiguration class be to readonly struct

--- RecordParser/Generic/MappingConfiguration.cs
+++ RecordParser/Generic/MappingConfiguration.cs
@@ -3,7 +3,7 @@ using System.Linq.Expressions;
 
 namespace RecordParser.Generic
 {
-    public class MappingConfiguration
+    public readonly struct MappingConfiguration
     {
         public MemberExpression prop { get; }
         public int start { get; }
