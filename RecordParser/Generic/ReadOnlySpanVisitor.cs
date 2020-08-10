a860918b99d1d1f0c4e6c0eda375f791ac683ecd
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2020-08-10 19:36:01 -0300

    moved ReadOnlySpanVisitor to another file

---
+++ RecordParser/Generic/ReadOnlySpanVisitor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace RecordParser.Generic
+{
+    public class ReadOnlySpanVisitor : ExpressionVisitor
+    {
+        public readonly ParameterExpression span = Expression.Parameter(typeof(ReadOnlySpan<char>), "span");
+
+        public delegate T FuncConvert<T>(ReadOnlySpan<char> text);
+
+        public Expression<FuncConvert<T>> Modify<T>(Expression<Func<ReadOnlySpanChar, T>> ex)
+        {
+            if (ex is null) return null;
+
+            var body = Visit(ex.Body);
+
+            var lamb = Expression.Lambda<FuncConvert<T>>(body, span);
+
+            return lamb;
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            return span;
+        }
+
+        protected override Expression VisitUnary(UnaryExpression node)
+        {
+            if (node.NodeType == ExpressionType.Convert
+             && node.Type == span.Type
+             && node.Operand.Type == typeof(ReadOnlySpanChar))
+                return span;
+
+            return base.VisitUnary(node);
+        }
+    }
+}
