8d5410a3c1029f853cfd1e15bcdbba7cf3ef5f70
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2020-12-25 04:00:12 -0300

    using ParseChunk strategy for both string and span

--- RecordParser/Parsers/CSVReader.cs
+++ RecordParser/Parsers/CSVReader.cs
@@ -28,27 +28,7 @@ namespace RecordParser.Parsers
 
         public T Parse(string str)
         {
-            Span<int> positions = stackalloc int[nth + 2];
-
-            var i = 0;
-            foreach (var x in GenericRecordParser.IndexOfNth(str, delimiter, nth + 2))
-            {
-                positions[i++] = x;
-            }
-
-            if (i < positions.Length)
-                throw new InvalidOperationException("menos colunas do q devia");
-
-            i = 0;
-            var csv = new string[config.Length];
-            
-            foreach (var index in config)
-            {
-                var start = positions[index];
-                var length = positions[index + 1] - start - delimiter.Length;
-                csv[i++] = str.Substring(start, length).Trim();
-            }
-
+            var csv = GenericRecordParser.IndexOfNth(str, delimiter, config, nth + 1);
             var result = parser(csv);
             return result;
         }
