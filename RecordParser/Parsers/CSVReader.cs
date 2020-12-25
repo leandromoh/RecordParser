b99e6906120638e4e2f8d89e99553738e3123b99
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2020-12-25 15:35:27 -0300

    moved GenericRecordParser.IndexOfNth to CSVReader and SpanCSVReader

--- RecordParser/Parsers/CSVReader.cs
+++ RecordParser/Parsers/CSVReader.cs
@@ -28,9 +28,43 @@ namespace RecordParser.Parsers
 
         public T Parse(string str)
         {
-            var csv = GenericRecordParser.IndexOfNth(str, delimiter, config, nth + 1);
-            var result = parser(csv);
+            string[] csv = IndexOfNth(str, delimiter, config, nth + 1);
+            T result = parser(csv);
             return result;
+        }
+
+        private static string[] IndexOfNth(string span, string delimiter, int[] config, int size)
+        {
+            var csv = new string[config.Length];
+            var scanned = -1;
+            var position = 0;
+            var j = 0;
+
+            for (var i = 0; i < size && j < config.Length; i++)
+            {
+                var (startIndex, length) = ParseChunk(ref span, ref scanned, ref position, delimiter);
+
+                if (i == config[j])
+                {
+                    csv[j] = span.Substring(startIndex, length).Trim();
+                    j++;
+                }
+            }
+
+            return csv;
+        }
+
+        private static (int, int) ParseChunk(ref string span, ref int scanned, ref int position, string delimiter)
+        {
+            scanned += position + 1;
+
+            position = span.IndexOf(delimiter, scanned) - scanned;
+            if (position < 0)
+            {
+                position = span.Length - scanned;
+            }
+
+            return (scanned, position);
         }
     }
 }
--- RecordParser/Parsers/SpanCSVReader.cs
+++ RecordParser/Parsers/SpanCSVReader.cs
@@ -28,9 +28,43 @@ namespace RecordParser.Parsers
 
         public T Parse(ReadOnlySpan<char> str)
         {
-            var csv = GenericRecordParser.IndexOfNth(str, delimiter, config, nth + 1);
-            var result = parser(str, csv);
+            (int, int)[] csv = IndexOfNth(str, delimiter, config, nth + 1);
+            T result = parser(str, csv);
             return result;
+        }
+
+        private static (int, int)[] IndexOfNth(ReadOnlySpan<char> span, ReadOnlySpan<char> delimiter, int[] config, int size)
+        {
+            var csv = new (int, int)[config.Length];
+            var scanned = -1;
+            var position = 0;
+            var j = 0;
+
+            for (var i = 0; i < size && j < config.Length; i++)
+            {
+                var range = ParseChunk(ref span, ref scanned, ref position, delimiter);
+
+                if (i == config[j])
+                {
+                    csv[j] = range;
+                    j++;
+                }
+            }
+
+            return csv;
+        }
+
+        private static (int, int) ParseChunk(ref ReadOnlySpan<char> span, ref int scanned, ref int position, ReadOnlySpan<char> delimiter)
+        {
+            scanned += position + 1;
+
+            position = span.Slice(scanned, span.Length - scanned).IndexOf(delimiter);
+            if (position < 0)
+            {
+                position = span.Slice(scanned, span.Length - scanned).Length;
+            }
+
+            return (scanned, position);
         }
     }
 }
