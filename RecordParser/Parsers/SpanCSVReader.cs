43e6b158614dfd2ff226eac2ede9e4c643d8af73
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2020-12-25 03:20:55 -0300

    all tests passing

--- RecordParser/Parsers/SpanCSVReader.cs
+++ RecordParser/Parsers/SpanCSVReader.cs
@@ -1,5 +1,6 @@
 ﻿using RecordParser.Generic;
 using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,27 +8,29 @@ namespace RecordParser.Parsers
 {
     public interface ISpanCSVReader<T>
     {
-        T Parse(string str);
+        T Parse(ReadOnlySpan<char> str);
     }
 
     public class SpanCSVReader<T> : ISpanCSVReader<T>
     {
-        private readonly Func<string, (int, int)[], T> parser;
+        private readonly FuncSpanArrayT<T> parser;
         private readonly int[] config;
         private readonly int nth;
+        private readonly string delimiter;
 
-        internal SpanCSVReader(IEnumerable<MappingConfiguration> list, Func<string, (int, int)[], T> parser)
+        internal SpanCSVReader(IEnumerable<MappingConfiguration> list, FuncSpanArrayT<T> parser)
         {
             config = list.Select(x => x.start).ToArray();
             nth = config.Max();
             this.parser = parser;
+            delimiter = ";";
         }
 
-        public T Parse(string str)
+        public T Parse(ReadOnlySpan<char> str)
         {
-            var csv = GenericRecordParser.IndexedSplit(str, ";", config, nth, ValueTuple.Create);
-
-            return parser(str, csv);
+            var csv = GenericRecordParser.IndexOfNth(str, delimiter, config, nth + 2);
+            var result = parser(str, csv);
+            return result;
         }
     }
 }
