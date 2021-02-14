a20c2bbbebb755d94f7e1d144ba77c21cc707b30
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2021-02-13 22:40:06 -0300

    adjust on Benchmark to reuse PipeReader flow

--- RecordParser.Benchmark/TestRunner.cs
+++ RecordParser.Benchmark/TestRunner.cs
@@ -19,16 +19,16 @@ namespace RecordParser.Benchmark
         [Params(100_000, 500_000)]
         public int LimitRecord { get; set; }
         
+        public string PathSampleData => Path.Combine(Directory.GetCurrentDirectory(), "SampleData.csv");
+
         [Benchmark]
         public async Task VariableLength_String_Raw()
         {
-            const int BufferSize = 128;
-            var path = Path.Combine(Directory.GetCurrentDirectory(), "SampleData.csv");
+            using var fileStream = File.OpenRead(PathSampleData);
+            using var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, bufferSize: 128);
+
             string line;
             var i = 0;
-
-            using var fileStream = File.OpenRead(path);
-            using var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize);
 
             while ((line = await streamReader.ReadLineAsync()) != null)
             {
@@ -61,11 +61,46 @@ namespace RecordParser.Benchmark
                 .Map(x => x.children, 7)
                 .Build(",");
 
-            var path = Path.Combine(Directory.GetCurrentDirectory(), "SampleData.csv");
+            await ProcessFile(parser.Parse);
+        }
+
+        [Benchmark]
+        public async Task VariableLength_Span_Raw()
+        {
+            await ProcessFile((ReadOnlySpan<char> line) =>
+            {
+                var scanned = -1;
+                var position = 0;
+
+                var id = ParseChunk(ref line, ref scanned, ref position);
+                var name = ParseChunk(ref line, ref scanned, ref position);
+                var age = ParseChunk(ref line, ref scanned, ref position);
+                var birthday = ParseChunk(ref line, ref scanned, ref position);
+                var gender = ParseChunk(ref line, ref scanned, ref position);
+                var email = ParseChunk(ref line, ref scanned, ref position);
+                ParseChunk(ref line, ref scanned, ref position);
+                var children = ParseChunk(ref line, ref scanned, ref position);
+
+                return new Person
+                {
+                    id = Guid.Parse(id),
+                    name = name.ToString(),
+                    age = int.Parse(age),
+                    birthday = DateTime.Parse(birthday, DateTimeFormatInfo.InvariantInfo),
+                    gender = Enum.Parse<Gender>(gender.ToString()),
+                    email = email.ToString(),
+                    children = bool.Parse(children)
+                };
+            });
+        }
+
+        public async Task ProcessFile(FuncSpanT<Person> parser)
+        {
+            using var stream = File.OpenRead(PathSampleData);
+            PipeReader reader = PipeReader.Create(stream);
+            
             var i = 0;
 
-            using var stream = File.OpenRead(path);
-            PipeReader reader = PipeReader.Create(stream);
             while (true)
             {
                 ReadResult read = await reader.ReadAsync();
@@ -83,161 +118,63 @@ namespace RecordParser.Benchmark
                     break;
                 }
             }
-
-            static bool TryReadLine(ref ReadOnlySequence<byte> buffer, out ReadOnlySequence<byte> line)
-            {
-                var position = buffer.PositionOf((byte)'\n');
-                if (position == null)
-                {
-                    line = default;
-                    return false;
-                }
-
-                line = buffer.Slice(0, position.Value);
-                buffer = buffer.Slice(buffer.GetPosition(1, position.Value));
-
-                return true;
-            }
-
-            static Person ProcessSequence(ReadOnlySequence<byte> sequence, IVariableLengthReader<Person> parser)
-            {
-                const int LengthLimit = 256;
-
-                if (sequence.IsSingleSegment)
-                {
-                    return Parse(sequence.FirstSpan, parser);
-                }
-
-                var length = (int)sequence.Length;
-                if (length > LengthLimit)
-                {
-                    throw new ArgumentException($"Line has a length exceeding the limit: {length}");
-                }
-
-                Span<byte> span = stackalloc byte[(int)sequence.Length];
-                sequence.CopyTo(span);
-
-                return Parse(span, parser);
-            }
-
-            static Person Parse(ReadOnlySpan<byte> bytes, IVariableLengthReader<Person> parser)
-            {
-                Span<char> chars = stackalloc char[bytes.Length];
-                Encoding.UTF8.GetChars(bytes, chars);
-
-                return parser.Parse(chars);
-            }
         }
 
-
-        [Benchmark]
-        public async Task VariableLength_Span_Raw()
+        static bool TryReadLine(ref ReadOnlySequence<byte> buffer, out ReadOnlySequence<byte> line)
         {
-            var path = Path.Combine(Directory.GetCurrentDirectory(), "SampleData.csv");
-            var i = 0;
-
-            using var stream = File.OpenRead(path);
-            PipeReader reader = PipeReader.Create(stream);
-            while (true)
+            var position = buffer.PositionOf((byte)'\n');
+            if (position == null)
             {
-                ReadResult read = await reader.ReadAsync();
-                ReadOnlySequence<byte> buffer = read.Buffer;
-                while (TryReadLine(ref buffer, out ReadOnlySequence<byte> sequence))
-                {
-                    if (i++ == LimitRecord) return;
-
-                    var person = ProcessSequence(sequence);
-                }
-
-                reader.AdvanceTo(buffer.Start, buffer.End);
-                if (read.IsCompleted)
-                {
-                    break;
-                }
+                line = default;
+                return false;
             }
 
-            static bool TryReadLine(ref ReadOnlySequence<byte> buffer, out ReadOnlySequence<byte> line)
+            line = buffer.Slice(0, position.Value);
+            buffer = buffer.Slice(buffer.GetPosition(1, position.Value));
+
+            return true;
+        }
+
+        static T ProcessSequence<T>(ReadOnlySequence<byte> sequence, FuncSpanT<T> parser)
+        {
+            const int LengthLimit = 256;
+
+            if (sequence.IsSingleSegment)
             {
-                var position = buffer.PositionOf((byte)'\n');
-                if (position == null)
-                {
-                    line = default;
-                    return false;
-                }
-
-                line = buffer.Slice(0, position.Value);
-                buffer = buffer.Slice(buffer.GetPosition(1, position.Value));
-
-                return true;
+                return Parse(sequence.FirstSpan, parser);
             }
 
-            static Person ProcessSequence(ReadOnlySequence<byte> sequence)
+            var length = (int)sequence.Length;
+            if (length > LengthLimit)
             {
-                const int LengthLimit = 256;
-
-                if (sequence.IsSingleSegment)
-                {
-                    return Parse(sequence.FirstSpan);
-                }
-
-                var length = (int)sequence.Length;
-                if (length > LengthLimit)
-                {
-                    throw new ArgumentException($"Line has a length exceeding the limit: {length}");
-                }
-
-                Span<byte> span = stackalloc byte[(int)sequence.Length];
-                sequence.CopyTo(span);
-
-                return Parse(span);
+                throw new ArgumentException($"Line has a length exceeding the limit: {length}");
             }
 
-            static Person Parse(ReadOnlySpan<byte> bytes)
-            {
-                Span<char> chars = stackalloc char[bytes.Length];
-                Encoding.UTF8.GetChars(bytes, chars);
+            Span<byte> span = stackalloc byte[(int)sequence.Length];
+            sequence.CopyTo(span);
 
-                return ParseLine(chars);
+            return Parse(span, parser);
+        }
+
+        static T Parse<T>(ReadOnlySpan<byte> bytes, FuncSpanT<T> parser)
+        {
+            Span<char> chars = stackalloc char[bytes.Length];
+            Encoding.UTF8.GetChars(bytes, chars);
+
+            return parser(chars);
+        }
+
+        static ReadOnlySpan<char> ParseChunk(ref ReadOnlySpan<char> span, ref int scanned, ref int position)
+        {
+            scanned += position + 1;
+
+            position = span.Slice(scanned, span.Length - scanned).IndexOf(',');
+            if (position < 0)
+            {
+                position = span.Slice(scanned, span.Length - scanned).Length;
             }
 
-            static Person ParseLine(ReadOnlySpan<char> span)
-            {
-                var scanned = -1;
-                var position = 0;
-
-                var id = ParseChunk(ref span, ref scanned, ref position);
-                var name = ParseChunk(ref span, ref scanned, ref position);
-                var age = ParseChunk(ref span, ref scanned, ref position);
-                var birthday = ParseChunk(ref span, ref scanned, ref position);
-                var gender = ParseChunk(ref span, ref scanned, ref position);
-                var email = ParseChunk(ref span, ref scanned, ref position);
-                _ = ParseChunk(ref span, ref scanned, ref position);
-                var children = ParseChunk(ref span, ref scanned, ref position);
-
-                return new Person
-                {
-                    id = Guid.Parse(id),
-                    name = name.ToString(),
-                    age = int.Parse(age),
-                    birthday = DateTime.Parse(birthday, DateTimeFormatInfo.InvariantInfo),
-                    gender = Enum.Parse<Gender>(gender.ToString()),
-                    email = email.ToString(),
-                    children = bool.Parse(children)
-                };
-            }
-
-            static ReadOnlySpan<char> ParseChunk(ref ReadOnlySpan<char> span, ref int scanned, ref int position)
-            {
-                scanned += position + 1;
-
-                position = span.Slice(scanned, span.Length - scanned).IndexOf(',');
-                if (position < 0)
-                {
-                    position = span.Slice(scanned, span.Length - scanned).Length;
-                }
-
-                return span.Slice(scanned, position);
-            }
+            return span.Slice(scanned, position);
         }
     }
 }
