add6b27a6c60d3a358a30d7c591659eb64ca5b22
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2021-02-15 23:22:13 -0300

    add BuilderWriter and Writer

---
+++ RecordParser/Parsers/VariableLengthWriter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RecordParser.Parsers
+{
+    public interface IVariableLengthWriter<T>
+    {
+        int Parse(T instance, Span<char> destination);
+    }
+
+    internal delegate int FuncSpanSpanIntT<T>(Span<char> span, ReadOnlySpan<char> delimiter, T inst);
+
+    internal class VariableLengthWriter<T> : IVariableLengthWriter<T>
+    {
+        private readonly FuncSpanSpanIntT<T> parse;
+        private readonly string separator;
+
+        public VariableLengthWriter(FuncSpanSpanIntT<T> parse, string separator)
+        {
+            this.parse = parse;
+            this.separator = separator;
+        }
+
+        public int Parse(T instance, Span<char> destination)
+        {
+            var charsWritten = parse(destination, separator, instance);
+            return charsWritten;
+        }
+    }
+}
