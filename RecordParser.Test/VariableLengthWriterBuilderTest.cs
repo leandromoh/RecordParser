4d9d9a8777a2c31d2cc3f3aafa611cd2160a72c7
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2021-02-16 02:23:04 -0300

    create more tests

--- RecordParser.Test/VariableLengthWriterBuilderTest.cs
+++ RecordParser.Test/VariableLengthWriterBuilderTest.cs
@@ -1,8 +1,6 @@
 ﻿using FluentAssertions;
 using RecordParser.BuilderWrite;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using Xunit;
 
 namespace RecordParser.Test
@@ -34,7 +32,7 @@ namespace RecordParser.Test
             var writer = new VariableLengthWriterBuilder<(string Name, DateTime Birthday, decimal Money, Color Color)>()
                 .Map(x => x.Name, indexColumn: 0)
                 .Map(x => x.Birthday, 1, "yyyy.MM.dd")
-                .Map(x => x.Color, 3)
+                .Map(x => x.Color, 4)
                 .Build(" ; ");
 
             var instance = ("foo bar baz", new DateTime(2020, 05, 23), 0123.45M, Color.LightBlue);
@@ -43,7 +41,63 @@ namespace RecordParser.Test
             var charsWritten = writer.Parse(instance, destination);
             var result = destination.Slice(0, charsWritten).ToString();
 
-            result.Should().Be("foo bar baz ; 2020.05.23 ;  ; LightBlue");
+            result.Should().Be("foo bar baz ; 2020.05.23 ;  ;  ; LightBlue");
+        }
+
+        [Fact]
+        public void Given_skip_first_column_using_standard_format_should_parse_without_extra_configuration()
+        {
+            var writer = new VariableLengthWriterBuilder<(string Name, DateTime Birthday, decimal Money, Color Color)>()
+                .Map(x => x.Name, indexColumn: 1)
+                .Map(x => x.Birthday, 2, "yyyy.MM.dd")
+                .Map(x => x.Money, 3)
+                .Map(x => x.Color, 4)
+                .Build(" ; ");
+
+            var instance = ("foo bar baz", new DateTime(2020, 05, 23), 0123.45M, Color.LightBlue);
+
+            Span<char> destination = stackalloc char[100];
+            var charsWritten = writer.Parse(instance, destination);
+            var result = destination.Slice(0, charsWritten).ToString();
+
+            result.Should().Be(" ; foo bar baz ; 2020.05.23 ; 123,45 ; LightBlue");
+        }
+
+        [Fact]
+        public void Given_skip2_first_column_using_standard_format_should_parse_without_extra_configuration()
+        {
+            var writer = new VariableLengthWriterBuilder<(string Name, DateTime Birthday, decimal Money, Color Color)>()
+                .Map(x => x.Name, indexColumn: 1)
+                .Map(x => x.Birthday, 2, "yyyy.MM.dd")
+                .Map(x => x.Money, 4)
+                .Map(x => x.Color, 5)
+                .Build(" ; ");
+
+            var instance = ("foo bar baz", new DateTime(2020, 05, 23), 0123.45M, Color.LightBlue);
+
+            Span<char> destination = stackalloc char[100];
+            var charsWritten = writer.Parse(instance, destination);
+            var result = destination.Slice(0, charsWritten).ToString();
+
+            result.Should().Be(" ; foo bar baz ; 2020.05.23 ;  ; 123,45 ; LightBlue");
+        }
+
+        [Fact]
+        public void Given_value_should_not_write_in_span_more_than_return_charswritten()
+        {
+            var writer = new VariableLengthWriterBuilder<(string Name, DateTime Birthday, decimal Money)>()
+                .Map(x => x.Name, indexColumn: 0)
+                .Map(x => x.Birthday, 1, "yyyy.MM.dd")
+                .Map(x => x.Money, 2)
+                .Build(" ; ");
+
+            var instance = ("foo bar baz", new DateTime(2020, 05, 23), 0123.45M);
+
+            Span<char> destination = stackalloc char[100];
+            var charsWritten = writer.Parse(instance, destination);
+            var result = destination.Slice(charsWritten).ToString();
+
+            result.Should().Be(new string(default, result.Length));
         }
     }
 }
