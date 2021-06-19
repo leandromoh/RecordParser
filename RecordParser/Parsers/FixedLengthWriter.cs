e4c1d5bb05ade337859951064116f741b9a13834
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2021-06-19 17:53:30 -0300

    added Parse as obsolete

--- RecordParser/Parsers/FixedLengthWriter.cs
+++ RecordParser/Parsers/FixedLengthWriter.cs
@@ -5,6 +5,9 @@ namespace RecordParser.Parsers
     public interface IFixedLengthWriter<T>
     {
         bool TryFormat(T instance, Span<char> destination, out int charsWritten);
+
+        [Obsolete("Method was renamed to TryFormat. Parse will eventually be removed in future release.")]
+        bool Parse(T instance, Span<char> destination, out int charsWritten);
     }
 
     public delegate (bool success, int charsWritten) FuncSpanTIntBool<T>(Span<char> span, T inst);
@@ -17,6 +20,10 @@ namespace RecordParser.Parsers
         {
             this.parse = parse;
         }
+
+        [Obsolete("Method was renamed to TryFormat. Parse will eventually be removed in future release.")]
+        public bool Parse(T instance, Span<char> destination, out int charsWritten) =>
+            TryFormat(instance, destination, out charsWritten);
 
         public bool TryFormat(T instance, Span<char> destination, out int charsWritten)
         {
