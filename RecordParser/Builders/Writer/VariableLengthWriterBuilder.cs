c50cab2823a2977743d4ec05e20d744669115bf0
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2021-06-21 19:18:45 -0300

    adds documentation for variablelength writer

--- RecordParser/Builders/Writer/VariableLengthWriterBuilder.cs
+++ RecordParser/Builders/Writer/VariableLengthWriterBuilder.cs
@@ -10,10 +10,54 @@ namespace RecordParser.Builders.Writer
 {
     public interface IVariableLengthWriterBuilder<T>
     {
+        /// <summary>
+        /// Creates the writer object using the registered mappings.
+        /// </summary>
+        /// <param name="separator">The text (usually a character) that delimits collumns and separate values.</param>
+        /// <param name="cultureInfo">Culture that will be used in the library internal default parsers functions.</param>
+        /// <remarks>
+        /// If a custom parser function was registered by the user (for member or type), 
+        /// this culture will not be applied. Culture should be manually applied in custom parse functions. 
+        /// </remarks>
+        /// <returns>The writer object.</returns>
         IVariableLengthWriter<T> Build(string separator, CultureInfo cultureInfo = null);
+
+        /// <summary>
+        /// Define a default custom function that will be used to parse all properties or fields of type <typeparamref name="R"/>,
+        /// except whose that were configurated with a specific custom function.
+        /// </summary>
+        /// <typeparam name="R">The type that will have a default custom function configurated.</typeparam>
+        /// <param name="ex">The default custom function for type <typeparamref name="R"/>.</param>
+        /// <returns>An object to configure the mapping.</returns>
         IVariableLengthWriterBuilder<T> DefaultTypeConvert<R>(FuncSpanTIntBool<R> ex);
 
+        /// <summary>
+        /// Customize configuration for individual member.
+        /// </summary>
+        /// <param name="ex">
+        /// An expression that identifies the property or field that will be assigned.
+        /// </param>
+        /// <param name="indexColumn">The zero-based position of the collumn where the field is located.</param>
+        /// <param name="format">
+        /// A standard or custom format for type <typeparamref name="R"/>.
+        /// </param>
+        /// <returns>An object to configure the mapping.</returns>
         IVariableLengthWriterBuilder<T> Map<R>(Expression<Func<T, R>> ex, int indexColumn, string format);
+
+        /// <summary>
+        /// Customize configuration for individual member.
+        /// </summary>
+        /// <param name="ex">
+        /// An expression that identifies the property or field that will be assigned.
+        /// </param>
+        /// <param name="indexColumn">The zero-based position of the collumn where the field is located.</param>
+        /// <param name="converter">
+        /// Custom function to write the member value into span.
+        /// The function must return 2 values to indicate if it was successful and how many characters were written.
+        /// The success value is used to decide if the write operation should advance to next field or be stopped.
+        /// The charsWritten value is used to calculate the number of non-written positions.
+        /// </param>
+        /// <returns>An object to configure the mapping.</returns>
         IVariableLengthWriterBuilder<T> Map<R>(Expression<Func<T, R>> ex, int indexColumn, FuncSpanTIntBool<R> converter = null);
     }
 
@@ -22,6 +66,20 @@ namespace RecordParser.Builders.Writer
         private readonly Dictionary<int, MappingWriteConfiguration> list = new();
         private readonly Dictionary<Type, Func<Expression, Expression, Expression, Expression>> dic = new();
 
+        /// <summary>
+        /// Customize configuration for individual member.
+        /// </summary>
+        /// <param name="ex">
+        /// An expression that identifies the property or field that will be assigned.
+        /// </param>
+        /// <param name="indexColumn">The zero-based position of the collumn where the field is located.</param>
+        /// <param name="converter">
+        /// Custom function to write the member value into span.
+        /// The function must return 2 values to indicate if it was successful and how many characters were written.
+        /// The success value is used to decide if the write operation should advance to next field or be stopped.
+        /// The charsWritten value is used to calculate the number of non-written positions.
+        /// </param>
+        /// <returns>An object to configure the mapping.</returns>
         public IVariableLengthWriterBuilder<T> Map<R>(Expression<Func<T, R>> ex, int indexColumn, FuncSpanTIntBool<R> converter = null)
         {
             var member = ex.Body as MemberExpression ?? throw new ArgumentException("Must be member expression", nameof(ex));
@@ -30,6 +88,17 @@ namespace RecordParser.Builders.Writer
             return this;
         }
 
+        /// <summary>
+        /// Customize configuration for individual member.
+        /// </summary>
+        /// <param name="ex">
+        /// An expression that identifies the property or field that will be assigned.
+        /// </param>
+        /// <param name="indexColumn">The zero-based position of the collumn where the field is located.</param>
+        /// <param name="format">
+        /// A standard or custom format for type <typeparamref name="R"/>.
+        /// </param>
+        /// <returns>An object to configure the mapping.</returns>
         public IVariableLengthWriterBuilder<T> Map<R>(Expression<Func<T, R>> ex, int indexColumn, string format)
         {
             var member = ex.Body as MemberExpression ?? throw new ArgumentException("Must be member expression", nameof(ex));
@@ -38,12 +107,29 @@ namespace RecordParser.Builders.Writer
             return this;
         }
 
+        /// <summary>
+        /// Define a default custom function that will be used to parse all properties or fields of type <typeparamref name="R"/>,
+        /// except whose that were configurated with a specific custom function.
+        /// </summary>
+        /// <typeparam name="R">The type that will have a default custom function configurated.</typeparam>
+        /// <param name="ex">The default custom function for type <typeparamref name="R"/>.</param>
+        /// <returns>An object to configure the mapping.</returns>
         public IVariableLengthWriterBuilder<T> DefaultTypeConvert<R>(FuncSpanTIntBool<R> ex)
         {
             dic.Add(typeof(R), ex?.WrapInLambdaExpression());
             return this;
         }
 
+        /// <summary>
+        /// Creates the writer object using the registered mappings.
+        /// </summary>
+        /// <param name="separator">The text (usually a character) that delimits collumns and separate values.</param>
+        /// <param name="cultureInfo">Culture that will be used in the library internal default parsers functions.</param>
+        /// <remarks>
+        /// If a custom parser function was registered by the user (for member or type), 
+        /// this culture will not be applied. Culture should be manually applied in custom parse functions. 
+        /// </remarks>
+        /// <returns>The writer object.</returns>
         public IVariableLengthWriter<T> Build(string separator, CultureInfo cultureInfo = null)
         {
             var maps = MappingWriteConfiguration.Merge(list.Select(x => x.Value), dic);
