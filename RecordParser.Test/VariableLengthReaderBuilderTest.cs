db69358911c9a9f641e5c3c700acf4a098250cb9
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2021-08-16 00:26:27 -0300

    add more tests

--- RecordParser.Test/VariableLengthReaderBuilderTest.cs
+++ RecordParser.Test/VariableLengthReaderBuilderTest.cs
@@ -2,6 +2,7 @@
 using RecordParser.Builders.Reader;
 using RecordParser.Parsers;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
@@ -48,6 +49,24 @@ namespace RecordParser.Test
         }
 
         [Fact]
+        public void Given_quoted_field_with_trailing_quotes()
+        {
+            var reader = new VariableLengthReaderBuilder<(int Year, string Model, string Comment, decimal Price)>()
+                .Map(x => x.Year, 0)
+                .Map(x => x.Model, 1)
+                .Map(x => x.Comment, 2)
+                .Map(x => x.Price, 3)
+                .Build(",");
+
+            var result = reader.Parse("1997,Ford,\"\"\"It is fast\"\"\",30100.99");
+
+            result.Should().BeEquivalentTo((Year: 1997,
+                                            Model: "Ford",
+                                            Comment: "\"It is fast\"",
+                                            Price: 30100.99));
+        }
+
+        [Fact]
         public void Given_skip_quoted_field()
         {
             var reader = new VariableLengthReaderBuilder<(int Year, string Model, string Comment, decimal Price)>()
@@ -60,7 +79,7 @@ namespace RecordParser.Test
 
             result.Should().BeEquivalentTo((Year: 1997,
                                             Model: "Ford",
-                                            Comment: (string) null,
+                                            Comment: (string)null,
                                             Price: 30100.99));
         }
 
@@ -116,6 +135,38 @@ namespace RecordParser.Test
                                             Comment: "Super, \"luxurious\" truck",
                                             Price: 30100.99));
         }
+
+        public static IEnumerable<object[]> Given_empty_fields_should_parse_source()
+        {
+            foreach (var model in new[] { "Ford", string.Empty })
+                foreach (var comment in new[] { "\"Super, \"\"luxurious\"\" truck\"", "new car", string.Empty })
+                    foreach (var owner in new[] { "Bob", string.Empty })
+                        yield return new object[] { model, comment, owner };
+        }
+
+        [Theory]
+        [MemberData(nameof(Given_empty_fields_should_parse_source))]
+        public void Given_empty_fields_should_parse(string model, string comment, string owner)
+        {
+            var reader = new VariableLengthReaderBuilder<(string Model, int Year, string Comment, decimal Price, string Owner)>()
+                .Map(x => x.Model, 0)
+                .Map(x => x.Year, 1)
+                .Map(x => x.Comment, 2)
+                .Map(x => x.Price, 3)
+                .Map(x => x.Owner, 4)
+                .Build(",");
+
+            var result = reader.Parse($"{model},1997,{comment},30100.99,{owner}");
+
+            result.Should().BeEquivalentTo((Model: model,
+                                            Year: 1997,
+                                            Comment: NormalizeQuotedField(comment),
+                                            Price: 30100.99,
+                                            Owner: owner));
+        }
+
+        private static string NormalizeQuotedField(string text) =>
+            text.Trim('"').Replace("\"\"", "\"");
 
         [Fact]
         public void Given_factory_method_should_invoke_it_on_parse()
