8e8567066c33a6cc43b6c28c0deaa964e30f36fb
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2021-08-29 01:48:27 -0300

    create Quote

---
+++ RecordParser.Test/VariableLengthWriterBuilderQuotedFieldTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecordParser.Test
+{
+    public partial class VariableLengthWriterBuilderFieldTest
+    {
+
+    }
+}
