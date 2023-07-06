07852a6b43f0cfae9bad631a8ca8e2dd30ec9d44
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2023-07-06 08:55:25 -0300

    adds documentation for IFL interface

--- RecordParser/Extensions/FileReader/IFL.cs
+++ RecordParser/Extensions/FileReader/IFL.cs
@@ -5,7 +5,18 @@ namespace RecordParser.Extensions.FileReader
 {
     internal interface IFL : IDisposable
     {
+        /// <summary>
+        /// Reads the file and fills the internal buffer.
+        /// </summary>
+        /// <returns>
+        /// The number of characters that have been read. 
+        /// This method returns zero when no more characters are left to read.
+        /// </returns>
         int FillBuffer();
+
+        /// <summary>
+        /// Read and returns the records inside the buffer.
+        /// </summary>
         IEnumerable<ReadOnlyMemory<char>> ReadLines();
     }
 }
