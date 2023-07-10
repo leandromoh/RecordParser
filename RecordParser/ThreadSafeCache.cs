431cac8c3317d31fe1823391fd2923fd0357f5a3
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2023-07-09 21:39:36 -0300

    adds ThreadSafeCache

---
+++ RecordParser/ThreadSafeCache.cs
@@ -0,0 +1,48 @@
+﻿namespace RecordParser
+{
+    using System;
+    using System.Threading;
+
+    public delegate string StringFactory(ReadOnlySpan<char> text);
+
+    namespace RecordParser.Extensions.FileReader
+    {
+        public class ThreadSafeCache
+        {
+            private readonly int _maxParallelism;
+            private readonly Foo[] _pool;
+            private int _i;
+
+            public ThreadSafeCache(Func<StringFactory> fac, int maxParallelism)
+            {
+                _i = -1;
+                _maxParallelism = maxParallelism;
+                _pool = new Foo[maxParallelism];
+                for (int i = 0; i < maxParallelism; i++)
+                {
+                    _pool[i] = new Foo { lockObj = new object(), fac = fac() };
+                }
+            }
+
+            public string Get(ReadOnlySpan<char> text)
+            {
+                return Get(text, Interlocked.Increment(ref _i));
+            }
+
+            public string Get(ReadOnlySpan<char> text, int i)
+            {
+                var r = _pool[i % _maxParallelism];
+                lock (r.lockObj)
+                {
+                    return r.fac(text);
+                }
+            }
+
+            private class Foo
+            {
+                public object lockObj;
+                public StringFactory fac;
+            }
+        }
+    }
+}
