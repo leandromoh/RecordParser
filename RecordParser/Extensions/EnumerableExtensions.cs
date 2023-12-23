8213cc2dd799b36e6b2fe119c4f260ae043b5a04
Leandro Fernandes <leandromoh@hotmail.com>
2023-12-23 13:22:59 -0300

    refactor/file-extensions-writer (faster & less allocation) (#98)

---
+++ RecordParser/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace RecordParser.Extensions
+{
+    internal static class EnumerableExtensions
+    {
+        public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> source, int batchSize)
+        {
+            using var e = source.GetEnumerator();
+            var hasMore = true;
+            bool MoveNext() => hasMore && (hasMore = e.MoveNext());
+
+            while (MoveNext())
+                yield return Chunk(batchSize);
+
+            IEnumerable<T> Chunk(int countdown)
+            {
+                do
+                    yield return e.Current;
+                while (--countdown > 0 && MoveNext());
+            }
+        }
+    }
+}
