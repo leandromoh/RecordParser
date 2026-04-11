ea14e46efedce3994328915dd180119f6e9edf43
unknown <leandromoh@hotmail.com>
2026-04-11 02:30:26 -0300

    adds documentation to methods interface

--- RecordParser/Parsers/VariableLengthReader.cs
+++ RecordParser/Parsers/VariableLengthReader.cs
@@ -5,8 +5,46 @@ namespace RecordParser.Parsers
 {
     public interface IVariableLengthReader<T>
     {
+        /// <summary>
+        /// Converts a span of characters to object of type <typeparamref name="T"/>.
+        /// In case of failure in property mapping, the callback <paramref name="exceptionHandler"/>
+        /// is called, then parsing continues for the next property.
+        /// </summary>
+        /// <param name="line">The span of characters to parse.</param>
+        /// <param name="exceptionHandler">
+        /// Callback function to be invoked when any property mapping throws an exception.
+        /// The first parameter is the thrown exception.
+        /// The second parameter is the index of the property whose mapping failed.
+        /// </param>
+        /// <returns>
+        /// An object that is equivalent to the values contained in <paramref name="line"/>.
+        /// </returns>
         T Parse(ReadOnlySpan<char> line, Action<Exception, int> exceptionHandler);
+
+        /// <summary>
+        /// Converts a span of characters to object of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="line">The span of characters to parse.</param>
+        /// <returns>
+        /// An object that is equivalent to the values contained in <paramref name="line"/>.
+        /// </returns>
         T Parse(ReadOnlySpan<char> line);
+
+        /// <summary>
+        /// Converts a span of characters to object of type <typeparamref name="T"/>.
+        /// A return value indicates whether the operation succeeded.
+        /// </summary>
+        /// <param name="line">The span of characters to parse.</param>
+        /// <param name="result">
+        /// When this method returns, contains the result of successfully parsing <paramref name="line"/>
+        /// or an undefined value on failure.
+        /// </param>
+        /// <returns>
+        /// true if value was successfully parsed; otherwise, false.
+        /// </returns>
+        /// <remarks>
+        /// The parsing is interrupted at the first property mapping failure.
+        /// </remarks>
         bool TryParse(ReadOnlySpan<char> line, out T result);
 
         internal string Separator { get; }
